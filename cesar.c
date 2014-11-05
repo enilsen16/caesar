2faba1d7ff0c26f905a2953fb4e5df4ff8f3c80e
Erik Nilsen <enilsen16@live.com>
2014-11-04 16:40:54 -0800

    add sudo code

--- cesar.c
+++ cesar.c
@@ -5,5 +5,15 @@
 
 int main(int argc, string argv[])
 {
+  // Prompt user for key(argc)
 
+  // Prompt user for message
+
+  // Store message as argv
+
+  // Convert each character to its ASCII value, then add the key to each
+
+  // Reconvert each new ASCII value back to a letter
+
+  // Print new message
 }
