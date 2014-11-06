8dc5c4ad1871ef9f5c5ec1f9455efc9318869b11
Erik Nilsen <enilsen16@live.com>
2014-11-05 16:55:36 -0800

    add validation for k

--- caesar.c
+++ caesar.c
@@ -8,13 +8,24 @@ int main(int argc, string argv[])
 {
   // store key in variable k
   int k = atoi(argv[1]);
-
-  // Prompt user for message
-  
-
-  // Store message as argv
-
+  string p;
+  printf("%d", argc);
+  if ( argc == 2 && k > 0)
+    {
+      // Prompt user for message and store in variable p
+      printf("What message would you like to encrypt?\n");
+      p = GetString();
+    }
+  else if ( k <= 0)
+    {
+      printf("Please enter a valid number that is greater than 1\n");
+    }
+  else
+    {
+      printf("Please enter only a number\n");
+    }
   // Convert each character to its ASCII value, then add the key to each
+  
 
   // Reconvert each new ASCII value back to a letter
 
